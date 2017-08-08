ebcb17f3c2955577fbd3563a86ca06e86971242b
gernest <geofreyernest@live.com>
2017-08-08 15:00:39 +0300

    Add Benchmark

--- variable_bench_test.go
+++ variable_bench_test.go
@@ -1,5 +1,13 @@
 package bearcub_test
 
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/gernest/bearcub"
+)
+
 // TODO: Add benchmarks
 /*
 
@@ -7,3 +15,30 @@ Instructions:
 
 
 */
+
+func BenchmarkReplace(b *testing.B) {
+	jr, err := bearcub.NewJSONReplacer([]byte("{}"))
+	if err != nil {
+		b.Fatal(err)
+	}
+	var buf bytes.Buffer
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+		s := []byte("{number}")
+		e := fmt.Sprint(i)
+		jr.O["number"] = i
+		buf.Reset()
+		b.StartTimer()
+		err := bearcub.ReplaceString(&buf, s, jr.Replace)
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.StopTimer()
+		if buf.String() != e {
+			b.Fatalf("expected %s got %s", e, buf.String())
+		}
+	}
+}
